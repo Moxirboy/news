bf2d896407a9e56baab6e445cc27ddd0197a1844
moxirboy <mox777boy@gmail.com>
2024-01-20 19:11:18 +0500

    added some functions on news

--- internal/service/repo/postgres/news.go
+++ internal/service/repo/postgres/news.go
@@ -9,19 +9,20 @@ import (
 )
 
 type newsRepository struct {
-	db *sql.DB
+	db     *sql.DB
 	logger logger.Logger
 }
-func NewNewsRepo(db *sql.DB,logger logger.Logger) repo.INewsRepository{
+
+func NewNewsRepo(db *sql.DB, logger logger.Logger) repo.INewsRepository {
 	return &newsRepository{
-		db:db,
-		logger:logger,
+		db:     db,
+		logger: logger,
 	}
 }
 func (r *newsRepository) Create(
 	ctx context.Context,
 	news *models.News,
-	) (newsID string, err error){
+) (newsID string, err error) {
 	err = r.db.QueryRowContext(
 		ctx,
 		NewsCreate,
@@ -36,4 +37,132 @@ func (r *newsRepository) Create(
 	}
 	return newsID, nil
 }
-	
+func (r *newsRepository) GetByID(
+	ctx context.Context,
+	ID string) (
+	*models.News, error,
+) {
+	news := models.News{}
+	err := r.db.QueryRowContext(
+		ctx,
+		NewsGetById,
+		ID,
+	).Scan(
+		&news.Title,
+		&news.Content,
+		&news.CreatedBy,
+		&news.Category,
+		&news.CreatedAt,
+	)
+	if err != nil {
+		r.logger.Error("repo.news.GetById Error:", err)
+		return nil, err
+	}
+	return &news, nil
+}
+func (r *newsRepository) GetAll(
+	ctx context.Context,
+) (
+	news []models.News,
+	err error,
+) {
+	rows, err := r.db.QueryContext(
+		ctx,
+		NewsGetAll,
+		10,
+	)
+	if err != nil {
+		r.logger.Error("repo.news.GetAll Error: ", err)
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		new := models.News{}
+		rows.Scan(
+			&new.ID,
+			&new.Title,
+			&new.Content,
+			&new.CreatedBy,
+			&new.Category,
+			&new.CreatedAt,
+		)
+		news = append(news, new)
+	}
+	return news, nil
+}
+func (r *newsRepository) Update(
+	ctx context.Context,
+	news *models.News,
+) error {
+	tx, err := r.db.BeginTx(
+		context.Background(),
+		&sql.TxOptions{
+			Isolation: sql.LevelSerializable,
+		},
+	)
+	if err != nil {
+		r.logger.Error("repo.news.update error while transaction begin:", err)
+		return err
+	}
+
+	res, execErr := tx.ExecContext(
+		ctx,
+		NewsUpdate,
+		news.Title,
+		news.Content,
+		news.CreatedBy,
+		news.Category,
+		news.UpdatedAt,
+		news.ID,
+	)
+	if execErr != nil {
+		r.logger.Error(
+			"repo.box.udpate error while insert box:",
+			execErr.Error(),
+		)
+		_ = tx.Rollback()
+		return execErr
+	}
+
+	if count, _ := res.RowsAffected(); count == 0 {
+		_ = tx.Rollback()
+		return sql.ErrNoRows
+	}
+	return nil
+}
+func (r *newsRepository) Delete(
+	ctx context.Context,
+	id string,
+) error {
+	tx, err := r.db.BeginTx(
+		context.Background(),
+		&sql.TxOptions{
+			Isolation: sql.LevelSerializable,
+		},
+	)
+	if err != nil {
+		r.logger.Error("repo.news.update error while transaction begin:", err)
+		return err
+	}
+
+	res, execErr := tx.ExecContext(
+		ctx,
+		NewsUpdate,
+		id,
+	)
+	if execErr != nil {
+		r.logger.Error(
+			"repo.box.udpate error while insert box:",
+			execErr.Error(),
+		)
+		_ = tx.Rollback()
+		return execErr
+	}
+
+	if count, _ := res.RowsAffected(); count == 0 {
+		_ = tx.Rollback()
+		return sql.ErrNoRows
+	}
+	return nil
+
+}
--- internal/service/repo/postgres/news_queries.go
+++ internal/service/repo/postgres/news_queries.go
@@ -1,11 +1,45 @@
 package postgres
 
-const(
+const (
 	NewsCreate = `
 		INSERT INTO news(
 		title,content,author,category,publication_date)	
 		VALUES($1,$2,$3,$4,$5)
 		RETURNING id
 		`
-	
+	NewsGetById = `
+	SELECT
+	title,
+	content,
+	author,
+	category,
+	publication_date
+	FROM news WHERE id=$1
+    WHERE deletedAt NULL
+	`
+	NewsGetAll = `
+SELECT
+id,
+title,
+content,
+author,
+category,
+publication_date
+FROM news LIMIT 10 OFFSET $1
+WHERE deletedAt NULL
+`
+	NewsUpdate = `
+	Update news
+SET title=$1,
+content=$2,
+author=$3,
+category=$4,
+updatedAt=CURRENT_TIMESTAMP
+where id=$5
+	`
+	NewsDelete = `
+	UPDATE news
+SET DeletedAt=CURRENT_TIMESTAMP
+WHERE id=$1
+`
 	)
