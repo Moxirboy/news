3ce9b93a7b243724ba5e94e07568fbb2e9fad8fd
moxirboy <mox777boy@gmail.com>
2024-01-20 19:14:00 +0500

    interface implementation

--- internal/service/repo/news.go
+++ internal/service/repo/news.go
@@ -10,5 +10,23 @@ type INewsRepository interface {
 		ctx context.Context,
 		news *models.News,
 	) (newsID string, err error)
-	
+	GetByID(
+		ctx context.Context,
+		ID string) (
+		*models.News, error,
+		)
+	GetAll(
+		ctx context.Context,
+		) (
+		news []models.News,
+		err error,
+		)
+	Update(
+		ctx context.Context,
+		news *models.News,
+		) error
+	Delete(
+		ctx context.Context,
+		id string,
+		) error
 }
